c928c3b34a61c6dcb90ef7d7cd75225eaad6ef86
GuilhermeBortoluz <guilhermechedidbortoluz@gmail.com>
2024-09-21 19:30:03 -0300

    Calculate area of triangle
    Calculate the area of any triangle using only information about the sides and the height.

---
+++ Geometry/Calculate area of triangle/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+using namespace std;
+
+double area, base=0, n2=0, n3=0, h=0,repetir=1.0;
+void menu(){
+    cout << "Digite o valor da base do triangulo, se nao tiver esta medida digite 0" << endl;
+    cin >> base;
+    while (base <0){
+        cout  << "Insira valor valido, nao sao aceitos numeros negativos" << endl;
+        cin >> base;
+    }
+    cout << "Digite o lado 2, se nao tiver esta medida digite 0" << endl;
+    cin >> n2;
+    while ( n2<0){
+        cout  << "Insira valor valido, nao sao aceitos numeros negativos" << endl;
+        cin >> n2;
+    }
+    cout << "Digite o lado 3, se nao tiver esta medida digite 0" << endl;
+    cin >> n3;
+    while (n3 <0){
+        cout  << "Insira valor valido, nao sao aceitos numeros negativos" << endl;
+        cin >> n3;
+    }
+    cout << "Digite a altura do triangulo, se nao tiver esta medida digite 0" << endl;
+    cin >> h;
+    while (h <0){
+        cout  << "Insira valor valido, nao sao aceitos numeros negativos" << endl;
+        cin >> h;
+    }
+    }
+bool test_trinagulo(){
+    if ((base+n2) > n3 and (base+n3) > n2 and (n2+n3) > base){
+        return true;
+    }
+    else{
+        cout << "Esse triangulo nao existe, insira novamente os valores" << endl;
+        return false;
+    };
+}   
+double equilatero(){
+    area = ((pow(base,2))*pow(3.0,0.5))/4;
+    return area;
+}
+double isosceles(){
+    area = (base*h)/2;
+    return area;
+}
+double heron(){
+    area = sqrt(((base+n2+n3)/2)*(((base+n2+n3)/2)-base)*(((base+n2+n3)/2)-n2)*(((base+n2+n3)/2)-n3));
+    return area;
+}
+int main(){
+    system("cls");
+    cout << "Bem vindo a calculadora de area de triangulo"<<endl;
+    while (repetir==1)
+    {
+        menu();
+        while (test_trinagulo() == false){
+            menu();
+            test_trinagulo();
+        }
+        if ((base==n2) and (n2==n3)){
+            cout <<"Area = " << equilatero() << endl;
+        }
+        else if ((base!=0)and h!=0){
+
+            cout << "Area = " << isosceles() << endl;
+
+        }
+        if (base!=0 and n2 !=0 and n3!=0 and (((base!=n2) and (base!=n3) and (n2!=n3)) and h==0))
+        {
+            cout << "Area = " << heron() << endl;
+
+        }
+        cout << "Digite 0 para para sair do programa, digite 1 para fazer outro calculo" << endl;
+        cin >> repetir;
+    }
+    cout << "Ate a proxima" << endl;
+    return 0;
+}
